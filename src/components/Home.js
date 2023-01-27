2cbab9eb05fcc8a07ccb7aa4fe94b83d90b5cbf4
merv-e <34655454+merv-e@users.noreply.github.com>
2023-01-27 20:20:30 +0100

    trying to figure out where to conditionally render the h2

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,29 +4,50 @@ import Polls from "./Polls";
 const Home = (props) => {
   // console.log(props.authedUser);  undefined.
 
+    // h2 will be either new or completed. 
+
+    // {/* {
+    //   props.hasUserVoted && ( */}
+    // )}
+    
   return (
     <div>
       <h2 className="center">New Polls</h2>  
       <ul className="container">
       {
-        props.questionIds.map(qid => (
-          <li className="poll-list" key={qid}>
-            <Polls id={qid} /> 
+        props.questionIds.map(qIdUnanswered => 
+        (
+          <li className="poll-list" key={qIdUnanswered}>
+            <Polls id={qIdUnanswered} /> 
           </li>
         ))
       }
       </ul>
-      <h2 className="center">Completed Polls</h2>
+    {/* <h2 className="center">Completed Polls</h2> */}
     </div>
   )
 };
 
-const mapStateToProps = ({questions}) => ({
+const mapStateToProps = ({questions, authedUser}) => ({
     questionIds : Object.keys(questions)
      .sort((x, y) => 
         questions[y].timestamp - questions[x].timestamp
-     )
+     ),
+    // hasUserVoted: !questions.id.optionOne.votes.includes(authedUser) || !questions.id.optionTwo.votes.includes(authedUser) //meaning the user hasn't voted.
+     
     //TODO : timestamps are not defined yet. Maybe create a helper file, like in the Udacity course?
 });
 
 export default connect(mapStateToProps)(Home)
+
+
+
+
+    //   {/* {
+    //   !props.questions.id.optionOne.votes.includes(props.authedUser) || !props.questions.id.optionTwo.votes.includes(props.authedUser) 
+    //   ? 
+    //   <h2 className="center">New Polls</h2>
+    //   :
+    //   <h2 className="center">Completed Polls</h2>
+    // } */}
+    
