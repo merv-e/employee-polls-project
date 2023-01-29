c115bb82af00215dd0f5e2b7f566fda14431f1c7
merv-e <34655454+merv-e@users.noreply.github.com>
2023-01-29 19:48:37 +0100

    .

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -12,7 +12,7 @@ export function handleLogin(userName, password) {
 
         const {users} = getState();
 
-        const userLogin = Object.values(users).find(u => u.id === userName && u.password === password)
+        const userLogin = Object.values(users).includes(u => u.id === userName && u.password === password)
         
         if (userLogin) {
             return dispatch(setAuthedUser(userLogin))
