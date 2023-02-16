0924864fadcb7aab223348e4e76278bf081dbbe6
merv-e <34655454+merv-e@users.noreply.github.com>
2023-02-16 22:22:38 +0100

    It's possible to toggle between new and completed polls now.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,73 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 import Polls from "./Polls";
 
 const Home = (props) => {
-  const hasUserVoted = props.questionIds.filter(q => !props.answeredQuestionIds.includes(q));
-  const hasUserNotVoted = props.questionIds.filter(q => props.answeredQuestionIds.includes(q));
-  
-  return (
-    <div>
-    <h2 className="center">New Polls</h2>
-      <ul className="container">
-      {
-       hasUserVoted.map(qId => 
-          <li className="poll-list" key={qId}>
-           <Polls 
-             id={qId} 
-            />
-          </li>
-      )}
-      </ul>  
 
-    <h2 className="center">Completed Polls</h2>
-      <ul className="container">
-      {
+  const [poll, setPoll] = useState("New Polls");  
+  const [toggle, setToggle] = useState(true); 
+  
+  const handleClick = () => {
+    setToggle(toggle => !toggle);
+
+    if (!toggle){ 
+      return setPoll("New Polls");
+    }
+    else if (toggle) {
+      return setPoll("Completed Polls")
+    };
+  };
+
+  // console.log(toggle); 
+  // console.log(poll);  
+  
+  const hasUserVoted = props.questionIds.filter(q => props.answeredQuestionIds.includes(q));
+  const hasUserNotVoted = props.questionIds.filter(q => !props.answeredQuestionIds.includes(q));
+
+  return (
+    <div className="center" > 
+      <h2 className="center">{poll}</h2> 
+    {
+       toggle
+      ? ( 
+        <ul className="container">
+        {
         hasUserNotVoted.map(qId => 
-          <li className="poll-list" key={qId}>
+            <li className="poll-list" key={qId}>
             <Polls 
-             id={qId} 
+              id={qId} 
               />
-          </li>
-      )}
-      </ul>  
+            </li>
+        )}
+        </ul>
+
+      )
+      : (
+        <ul className="container">
+        {
+          hasUserVoted.map(qId => 
+            <li className="poll-list" key={qId}>
+              <Polls 
+              id={qId} 
+                />
+            </li>
+        )}
+        </ul>
+    )
+  }
+      <button 
+        className="btn btn-dark" 
+        onClick={handleClick}
+        >
+        Switch to
+        {
+          poll === "New Polls" 
+          ? " Completed Polls"
+          : poll === "Completed Polls"
+          ? " New Polls"
+          : null
+          } 
+        </button> 
     </div>
   )
 };
