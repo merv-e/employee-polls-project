1d01a00007052738ce4466b162c4f5c5e0d37941
merv-e <34655454+merv-e@users.noreply.github.com>
2023-02-16 22:32:41 +0100

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,13 +16,21 @@ const Home = (props) => {
     else if (toggle) {
       return setPoll("Completed Polls")
     };
-  };
-
-  // console.log(toggle); 
-  // console.log(poll);  
-  
+  }; 
+  //the user hasn't been voted yet, therefore the poll will be in the new polls.
   const hasUserVoted = props.questionIds.filter(q => props.answeredQuestionIds.includes(q));
+
+  //the user has voted, so the poll will be under in the completed polls.
   const hasUserNotVoted = props.questionIds.filter(q => !props.answeredQuestionIds.includes(q));
+
+  // TODO: ?  These lines and conditionally rendering options can be added.
+
+  // if (hasUserNotVoted.length === 0) {
+  //   return <p>You haven't voted for any of the polls yet. Would you like to consider checking the polls?</p>
+  // }
+  // if (hasUserVoted.length === 0 ) {
+  //   return <p>There are no new questions.</p>
+  // }
 
   return (
     <div className="center" > 
