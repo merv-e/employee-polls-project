dfb3fda188b642e9664517f386078473d92fcc70
merv-e <34655454+merv-e@users.noreply.github.com>
2023-02-17 14:16:29 +0100

    Revert "react testing libraries' locations in the package.json is  changed to devDependencies instead of dependencies"
    This reverts commit 1b89d0b0064b2ecff6c5791f6634f8686a27d145.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,16 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import *as React from "react";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i); 
-  expect(linkElement).toBeInTheDocument();
-});
-
-// describe("App", () => {
-//   it('should match the snapshot of the App', () => {
-//     const view = render(<App/>);
-//     expect(view).toMatchSnapshot(); 
-//   });
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
 // });
