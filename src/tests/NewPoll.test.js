d75056bd4f72badd70fc498bcbf1cd1c84caa120
merv-e <34655454+merv-e@users.noreply.github.com>
2023-02-20 16:16:03 +0100

    a very basic snapshot is written for New Poll component.

--- src/tests/NewPoll.test.js
+++ src/tests/NewPoll.test.js
@@ -3,42 +3,34 @@ import *as React from "react";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import NewPoll from "../components/NewPoll";
-// import store from "../index"; //store.js'den al :)
 import {store} from "../store";
 
-// test("should have the expected form elements", ()=> {
-//         const utils = render(
-//             // <Provider store={store}>
-//             // <MemoryRouter>
-//                     <NewPoll />
-//             // </MemoryRouter>
-//                 // </Provider>
-//         );
+    test ("should take a screenshot of the page", () => {
+        const utils = render(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <NewPoll />
+                </MemoryRouter>
+            </Provider>
+        );
         
-//         // screen.
-//         expect(utils).toMatchSnapshot();
-//         // expect(getByTestId("first-option-input")).toBeInTheDocument();
-        
-//         // const fOption = screen.getByTestId("first-option-input")
-//         // expect(fOption).toBeInTheDocument();
-// });
-
-
-// describe("NewPoll", ()=> {
-//     it('should have the expected form elements', () => {
-//         const utils = render(
-//             // <MemoryRouter>
-//             //     <Provider store={store}>
-//                     <NewPoll />
-//             //     </Provider>
+        expect(utils).toMatchSnapshot();
+    });
+    
+    // describe("NewPoll", ()=> {
+        //     it('should have the expected form elements', () => {
+            //         const utils = render(
+                //             // <MemoryRouter>
+                //             //     <Provider store={store}>
+                //                     <NewPoll />
+                //             //     </Provider>
 //             // </MemoryRouter>
 //         );
-        
-//         // screen.
-//         expect(utils).toMatchSnapshot();
-//         // expect(getByTestId("first-option-input")).toBeInTheDocument();
-        
-//         // const fOption = screen.getByTestId("first-option-input")
-//         // expect(fOption).toBeInTheDocument();
+
+// expect(getByTestId("first-option-input")).toBeInTheDocument();
+
+// const fOption = screen.getByTestId("first-option-input")
+// expect(fOption).toBeInTheDocument();
+
 //     });
 // });
