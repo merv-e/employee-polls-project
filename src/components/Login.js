6345bfe1508b7a65b1a322713c764cf6df66594d
merv-e <34655454+merv-e@users.noreply.github.com>
2023-03-27 16:46:07 +0200

    changes are made in  Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,70 +8,84 @@ import { withRouter } from "../utils/helpers";
 import Home from "./Home";
 import Leaderboard from "./Leaderboard";
 import NewPoll from "./NewPoll";
+import { Button } from "react-bootstrap";
 
 const Login = (props) => {
   
   const navigate = useNavigate();
   const {dispatch} = props;
   const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const authentification = props.userInfo.filter(u => u.id === username && u.password === password).map(u => u.id);
+  // const [password, setPassword] = useState("");
+  // const authentification = props.userInfo.filter(u => u.id === username && u.password === password).map(u => u.id);
   
-  const user = authentification.find(user => user);
+  // const user = authentification.find(user => user);
+
+  // const user = username;  
 
   //if the pathname is /login redirect to the homepage
   if (props.path === "/login") {
     navigate("/");
   }
   
-  const handleSubmit = (e) => { 
-    e.preventDefault();
+  const handleSubmit = event => { 
+    // e.preventDefault();
     dispatch(showLoading()); 
+
+    // const user = setUserName(ev.target.value)
+
+    // setUserName(event.target.value)
+
+    console.log(username);
     
+    const user = username;  
+
+    console.log(user);
+
     if (user) {
       dispatch(setAuthedUser(username));
     }
 
-    else alert("Incorrect password or username");
+    else alert("Incorrect password or username"); 
 
     setUserName("");
-    setPassword("");
+    // setPassword("");
   
   dispatch(hideLoading());
   };
 
   return (
-    <div className="login-page">
+    <>
       <img
        data-testid="login-avatar"
        className="loginImg" 
        src={loginImg} 
        alt="Employee polls"
-       />
-     <form 
-       className="login" 
-       onSubmit={handleSubmit} >
-        <input 
-         value={username}
-         data-testid="login-name"
-         placeholder="username" type="text"
-         onChange={(event) => setUserName(event.target.value)}/>
-       <input
-        data-testid="login-password"
-        value={password} 
-        placeholder="password" 
-        type="text"
-        onChange={(event) => setPassword(event.target.value)}/>
-        <button
-         data-testid="login-submit"
-         type="submit" 
-         disabled={
-          username === "" || password === ""
-         }
-        >Login
-        </button>
-      </form>
-    </div>
+      />
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+           <label htmlFor="default-option">Select User</label>
+          <select 
+           value={username}
+           className="form-control"
+           id="users"
+           onChange={
+            (event) => setUserName(event.target.value)} 
+           >
+            <option defaultValue disabled id="default-option"></option>
+            <option value="zoshikanlu">zoshikanlu</option>
+            <option value="tylermcginnis">tylermcginnis</option>
+            <option value="sarahedo">sarahedo</option>
+            <option value="mtsamis">mtsamis</option>
+          </select>
+        </div>
+        
+        <Button 
+         type="submit"
+         >Submit
+         </Button>
+      </form> 
+    </>
   )
 };
 
@@ -80,15 +94,15 @@ const Login = (props) => {
     const userInfo = Object.values(users)
     .map(u => ({
       id: u.id,
-      password: u.password
+      // password: u.password
     }));
 
-const path = props.router.location.pathname.replace("/question/", "");
+    const path = props.router.location.pathname.replace("/question/", "");
 
     return {
       userInfo,
       questionIds : Object.values(questions).map((q) => q.id),
-      path
+      path,
     };
 };
 
